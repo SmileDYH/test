6afe89f5d38d4e4fb5fb57a2f80d44fc0570e80f
dyh <574916501@qq.com>
2023-01-09 12:57:25 +0800

    feat():模板设计模式完善，新增钩子方法

--- src/main/java/com/dyh/test/design_mode/template/Template.java
+++ src/main/java/com/dyh/test/design_mode/template/Template.java
@@ -1,5 +1,4 @@
 package com.dyh.test.design_mode.template;
-
 import java.util.Calendar;
 
 /**
@@ -13,14 +12,32 @@ public class Template {
 
         //季度分享电影短片开始 顺序回顾每个人的分享流程
         ShareTemp xiaojieShare = new XiaoJieShare();
-        xiaojieShare.share(calendar.getTime(),"三楼接待室");
+        xiaojieShare.share(calendar.getTime(),"三楼接待室【jie】");
 
         ShareTemp xiaochenshare = new XiaoChenShare();
         calendar.set(Calendar.DAY_OF_MONTH,10);
-        xiaochenshare.share(calendar.getTime(),"总裁办公室");
+        xiaochenshare.share(calendar.getTime(),"总裁办公室【chen】");
 
         ShareTemp xiaolongShare = new XiaoLongShare();
         calendar.set(Calendar.HOUR_OF_DAY,23);
-        xiaolongShare.share(calendar.getTime(),"宇宙中心");
+        xiaolongShare.share(calendar.getTime(),"宇宙中心【long】");
+
+        /* -------------------------匿名内部类方式---------------------------*/
+        AShare aShare = new AShare();
+        aShare.bond(new ShareTemp() {
+            //匿名内部类实现回调
+            @Override
+            void shareContent() {
+                System.out.println("2. 我是新来的小a，我今天分享的内容是...");
+            }
+            /**
+             * 重构钩子方法
+             */
+            @Override
+            public void hookMethod(){
+                //子类可以在父类钩子方法实现的基础上进行扩展
+                System.out.println("我是插队进来的小a");
+            }
+        },calendar.getTime(),"中科宏一【a】");
     }
 }
